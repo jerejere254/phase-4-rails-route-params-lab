191380ad16a8b3baaec8d55ea9a244659a2b9c0f
jerryjohns <jerry.johns@student.moringaschool.com>
2022-11-22 09:42:06 +0300

    commit tree 79c4b32a8255afae125b59e29674594c6a899941

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,12 @@
 class StudentsController < ApplicationController
-
   def index
     students = Student.all
     render json: students
+  end
+
+  def specific_student
+    specific = Student.all.find(params[:id])
+    render json: specific
   end
 
 end
